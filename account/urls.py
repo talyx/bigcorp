1b2a09fde54169050d3e6f813c3187fb4f38f6f2
talyx <farruh.rahmatov40@gmail.com>
2025-02-08 18:22:01 +0500

    feat: Add password reset templates and update login page
    - Added password reset templates to account app
    - Updated login template with a "Reset Password" option
    - Added URL patterns for password reset views
    - Ensured proper integration with authentication flow

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from django.shortcuts import render
 from . import views
 
@@ -19,5 +20,25 @@ urlpatterns = [
     path("dashboard", views.dashboard_user, name="dashboard"),
     path("profile-management", views.profile_user, name="profile-management"),
     path('delete-user/', views.delete_user, name='delete-user'),
+    
+    #Password Reset
+    path(
+        'passwrod-reset/', auth_views.PasswordResetView.as_view(
+            template_name='account/password/password_reset.html',
+            success_url=reverse_lazy('account:password-reset-done'),
+            email_template_name='account/password/password_reset_email.html',
+        ),
+        name='password-reset',
+    ),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password/password_reset_done.html',
+    ), name = 'password-reset-done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='account/password/password_reset_confirm.html',
+        success_url=reverse_lazy('account:password-reset-complete'),
+    ), name='password-reset-confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='account/password/password_reset_complete.html',
+    ), name='password-reset-complete'),
     
 ]
