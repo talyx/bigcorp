ef321ffe2e2542d8510fc30d24c060a3c4abd0d9
talyx <farruh.rahmatov40@gmail.com>
2025-02-08 17:28:05 +0500

    feat: Add ShippingAddressForm and order-related models
    - Created forms.py with ShippingAddressForm
    - Added models: Order, OrderItem, ShippingAddress
    - Defined relationships and fields for order processing
    - Prepared models for integration with the payment flow

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db import models
 
-# Create your models here.
+from shop.models import Product
+
+User = get_user_model()
+
+class ShippingAddress(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField( max_length=254)
+    
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    zip_code = models.CharField(max_length=100, blank=True, null=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "ShippingAdress object: " + str(self.id)
+    
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    shipping_address = models.ForeignKey(
+        ShippingAddress, on_delete=models.CASCADE, blank=True, null=True)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Order" + str(self.id)
+    
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, blank=True, null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    
+    def __str__(self):
+        return "OrderItem" + str(self.id)
