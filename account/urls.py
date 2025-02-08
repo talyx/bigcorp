0c21d592626e98da09dc03d65f707a845c7a3a0b
talyx <farruh.rahmatov40@gmail.com>
2025-02-08 14:40:27 +0500

    chore: adding external libraries for working with email
    - Installed external libraries `django-email-verification` and
    `django-crispy-forms`.
    - Added the libraries to the project's `requirements.txt` file.
    - Configured the project settings to include the new libraries.
    - Created a `.env` file to manage environment variables.
    - Updated the project settings to read from the `.env` file using
    `python-dotenv
    - Updated urls for correct work of the project

---
+++ account/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from django.shortcuts import render
+from . import views
+
+app_name = "account"
+
+urlpatterns = [
+    #Registration and verification
+    path("register", views.register_user, name="register"),
+    path(
+        "email-verification-sent/",
+        lambda request: render(request, "account/email/email-verification-sent.html"),
+        name="email-verification-sent",
+    ),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    
+    #Dashboard
+    path("dashboard", views.dashboard_user, name="dashboard"),
+    path("profile-management", views.profile_user, name="profile-management"),
+    path('delete-user/', views.delete_user, name='delete-user'),
+    
+]
--- bigcorp/settings.py
+++ bigcorp/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import environ
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+env.read_env(BASE_DIR / '.env')
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,9 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party
     'mathfilters',
+    'crispy_forms',
+    "crispy_bootstrap5",
+    'django_email_verification',
     # apps
     'shop',
     'cart',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +142,51 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+def email_verified_callback(user):
+    user.is_active = True
+
+
+# def password_change_callback(user, password):
+#     user.set_password(password)
+
+
+# Global Package Settings
+EMAIL_FROM_ADDRESS = 'bigcorp-django@gmil.com'  # mandatory
+EMAIL_PAGE_DOMAIN = 'http://localhost:8000/'  # mandatory (unless you use a custom link)
+EMAIL_MULTI_USER = False  # optional (defaults to False)
+
+# Email Verification Settings (mandatory for email sending)
+EMAIL_MAIL_SUBJECT = 'Confirm your email {{ user.username }}'
+EMAIL_MAIL_HTML = 'account/email/mail_body.html'
+EMAIL_MAIL_PLAIN = 'account/email/mail_body.txt'
+EMAIL_MAIL_TOKEN_LIFE = 60 * 60  # one hour
+
+# Email Verification Settings (mandatory for builtin view)
+EMAIL_MAIL_PAGE_TEMPLATE = 'account/email/email_success_template.html'
+EMAIL_MAIL_CALLBACK = email_verified_callback
+
+# # Password Recovery Settings (mandatory for email sending)
+# EMAIL_PASSWORD_SUBJECT = 'Change your password {{ user.username }}'
+# EMAIL_PASSWORD_HTML = 'password_body.html'
+# EMAIL_PASSWORD_PLAIN = 'password_body.txt'
+# EMAIL_PASSWORD_TOKEN_LIFE = 60 * 10  # 10 minutes
+
+# # Password Recovery Settings (mandatory for builtin view)
+# EMAIL_PASSWORD_PAGE_TEMPLATE = 'password_changed_template.html'
+# EMAIL_PASSWORD_CHANGE_PAGE_TEMPLATE = 'password_change_template.html'
+# EMAIL_PASSWORD_CALLBACK = password_change_callback
+
+# For Django Email Backend
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bigcorp-django@gmil.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # os.environ['password_key'] suggested
+EMAIL_USE_TLS = True
